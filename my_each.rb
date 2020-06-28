738b356e13447518f73955adb52a9b5030dbb119
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-28 18:34:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,12 +5,15 @@
 list = ["ab", "cd", "ef"]
 
 def my_each(array)
+  if block_given?
   i = 0
   while i < array.length
 # The "while" method will always return a value of => nil"
     yield(array[i])
     i = i + 1
   end
+  
+  array
 end
 
 my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
